3fcae7a3754dd4f01babb57ddf58751b10c697c6
NandaniPGajjar <npgajjar2812@gmail.com>
2024-07-25 12:33:06 +0530

    build on azure

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       onGenerateRoute: (settings) => RouterGenerate.generateRoute(settings),
       // home: IPSoolutionsUI(),
-      home: HomePage(),
+      // home: HomePage(),
     );
   }
 }
